9c443dc1ff5f084856d81d2cac34c07d1d76c954
zwendo <lorris.creantor@gmail.com>
2021-10-17 15:14:44 +0200

    Resolved wildcard imports.

--- src/main/java/com/notkamui/event/Event.java
+++ src/main/java/com/notkamui/event/Event.java
@@ -2,7 +2,14 @@ package com.notkamui.event;
 
 import com.notkamui.user.User;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 
--- src/main/java/com/notkamui/user/User.java
+++ src/main/java/com/notkamui/user/User.java
@@ -3,7 +3,13 @@ package com.notkamui.user;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.notkamui.event.Event;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
