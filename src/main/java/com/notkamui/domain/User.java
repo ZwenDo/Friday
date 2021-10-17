a8310a2b6e1717aa40438595764dc30315ac230d
zwendo <lorris.creantor@gmail.com>
2021-10-17 13:26:27 +0200

    Added user table class.

---
+++ src/main/java/com/notkamui/domain/User.java
@@ -0,0 +1,31 @@
+package com.notkamui.domain;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "user")
+public class User {
+    public User() {}
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
