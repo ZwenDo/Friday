8d0fddde0dd7ab0cc0a88d9b32a9e6cfb65a530a
notKamui <teillard.jimmy@outlook.fr>
2021-10-18 19:02:00 +0200

    UserRepository can now delete a user by id.

--- src/main/java/com/notkamui/user/UserRepository.java
+++ src/main/java/com/notkamui/user/UserRepository.java
@@ -1,7 +1,42 @@
 package com.notkamui.user;
 
+import javax.validation.constraints.NotNull;
 import java.security.NoSuchAlgorithmException;
+import java.util.Optional;
+import java.util.UUID;
 
+/**
+ * Interface used by Micronaut to inject a UserRepository when requested.
+ *
+ * Serves to manage the {@code user} table.
+ */
 public interface UserRepository {
-    User save(String username, String password) throws NoSuchAlgorithmException;
+    /**
+     * Finds a user by its id.
+     *
+     * SELECT * USER WHERE ID = {id} LIMIT 1;
+     *
+     * @param id the id of the user
+     * @return an optional of {@code User} (empty if not found)
+     */
+    Optional<User> findById(@NotNull UUID id);
+
+    /**
+     * Saves a new user.
+     *
+     * @param username the unique username of the user
+     * @param password the password of the user (will be hashed SHA-512)
+     * @return the created user
+     * @throws NoSuchAlgorithmException (should never happen unless SHA-512 doesn't exist anymore)
+     */
+    User save(@NotNull String username, @NotNull String password) throws NoSuchAlgorithmException;
+
+    /**
+     * Deletes a user by its id, provided the given password is the correct one.
+     *
+     * @param id the id of the user to delete
+     * @param password the password of the user to delete
+     * @return true if the user has been deleted successfully (known id + valid password), false otherwise.
+     */
+    boolean deleteById(@NotNull UUID id, @NotNull String password);
 }
--- src/main/java/com/notkamui/user/UserRepositoryImpl.java
+++ src/main/java/com/notkamui/user/UserRepositoryImpl.java
@@ -1,13 +1,17 @@
 package com.notkamui.user;
 
 import com.notkamui.utils.SHA512Hasher;
+import io.micronaut.transaction.annotation.ReadOnly;
 import jakarta.inject.Singleton;
 
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 import javax.validation.constraints.NotNull;
 import java.security.NoSuchAlgorithmException;
-import java.util.Objects;
+import java.util.Optional;
+import java.util.UUID;
+
+import static java.util.Objects.requireNonNull;
 
 @Singleton
 public class UserRepositoryImpl implements UserRepository {
@@ -16,18 +20,37 @@ public class UserRepositoryImpl implements UserRepository {
     private final SHA512Hasher hasher = SHA512Hasher.getHasher();
 
     public UserRepositoryImpl(@NotNull EntityManager manager) throws NoSuchAlgorithmException {
-        Objects.requireNonNull(manager);
+        requireNonNull(manager);
         this.manager = manager;
+    }
+
+    @Override
+    @ReadOnly
+    public Optional<User> findById(UUID id) {
+        return Optional.ofNullable(manager.find(User.class, id));
     }
 
     @Override
     @Transactional
     public User save(String username, String password) {
-        Objects.requireNonNull(username);
-        Objects.requireNonNull(password);
+        requireNonNull(username);
+        requireNonNull(password);
         var hashedPwd = hasher.hash(password);
         var user = new User(username, hashedPwd);
         manager.persist(user);
         return user;
+    }
+
+    @Override
+    @Transactional
+    public boolean deleteById(UUID id, String password) {
+        requireNonNull(id);
+        requireNonNull(password);
+        var hashedPwd = hasher.hash(password);
+        var user = findById(id);
+        if (user.isEmpty()) return false;
+        if (!user.get().password().equals(hashedPwd)) return false;
+        manager.remove(user);
+        return true;
     }
 }
