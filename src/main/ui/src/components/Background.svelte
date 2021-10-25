8ee2416f79fc7d2719fc9bb5367484d5bc205898
notKamui <teillard.jimmy@outlook.fr>
2021-10-25 12:36:52 +0200

    Replaced background rotation with clip-path to avoid weird anti-aliasing issues on Firefox.

--- src/main/ui/src/components/Background.svelte
+++ src/main/ui/src/components/Background.svelte
@@ -6,30 +6,24 @@
         width: 100vw;
         height: 100vh;
         position: absolute;
-        overflow: hidden;
-        z-index: -2;
         top: 0;
         left: 0;
+        overflow: hidden;
+        z-index: -2;
     }
 
     .background-inner {
-        transform: rotate(-12deg) scale(1.5) translateY(-45%);
-        -moz-transform: rotate(-12deg) scale(1.5) translateY(-45%);
-        -webkit-transform: rotate(-12deg) scale(1.5) translateY(-45%);
-        background: linear-gradient(90deg, #5a1e8f, #6411a9, #f369ad);
+        background: linear-gradient(90deg, #5a1e8f, #7c14d2, #f369ad);
         background-size: 300% 300%;
-        animation: gradient 5s alternate infinite;
-        position: absolute;
-        top: 0;
-        left: 0;
-        height: 60%;
-        width: 100%;
+        clip-path: polygon(0% 0%,100% 0%,100% 13%,0% 49%);
+        animation: gradient 10s alternate infinite;
+        height: 100%;
         z-index: -1;
     }
 
     @keyframes gradient {
         0% {
-            background-position: 0%;
+            background-position: 0;
         }
         100% {
             background-position: 100%;
--- src/main/ui/src/components/Navbar.svelte
+++ src/main/ui/src/components/Navbar.svelte
@@ -12,11 +12,15 @@
         font-size: 4em;
         margin: 0.2em;
         padding: 0;
+        -moz-transition: all 200ms ease-in-out;
+        -webkit-transition: all 200ms ease-in-out;
         transition: all 200ms ease-in-out;
         display: inline-block;
     }
 
     .head-title:hover {
+        -moz-transform: scale(1.1);
+        -webkit-transform: scale(1.1);
         transform: scale(1.1);
     }
 </style>
