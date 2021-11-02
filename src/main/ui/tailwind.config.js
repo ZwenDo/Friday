444a5ad53ba5f59271ee891c98bb2f76c761b530
notKamui <teillard.jimmy@outlook.fr>
2021-11-02 22:35:08 +0100

    Purge unused CSS and using tailwind's JIT compiler for faster builds.

--- src/main/ui/tailwind.config.js
+++ src/main/ui/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-    purge: [],
+    mode: 'jit',
+    purge: ['./src/**/*.{js,svelte}', './public/index.html'],
     darkMode: false, // or 'media' or 'class'
     theme: {
         fontFamily: {
