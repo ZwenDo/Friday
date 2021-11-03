0ef944aad4ad1bb448729d0b2a61d76a426e2119
notKamui <teillard.jimmy@outlook.fr>
2021-11-03 21:29:25 +0100

    Replaced thread loop with a more efficient and clean named scheduled executor.

--- src/main/java/com/kalia/friday/login/SessionManagerService.java
+++ src/main/java/com/kalia/friday/login/SessionManagerService.java
@@ -7,6 +7,8 @@ import org.slf4j.LoggerFactory;
 
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 import static java.util.Objects.requireNonNull;
 
@@ -16,7 +18,7 @@ import static java.util.Objects.requireNonNull;
 @Singleton
 public class SessionManagerService {
 
-    private static final long PURGE_DELAY = 10_800_000; // 3h
+    private static final long PURGE_DELAY = 5000;//10_800_000; // 3h
     private static final long TOKEN_LIFETIME_IN_DAYS = 7;
     private static final Logger LOGGER = LoggerFactory.getLogger(SessionManagerService.class);
 
@@ -36,25 +38,20 @@ public class SessionManagerService {
      * Starts the service and periodically checks for expired tokens.
      */
     public void start() {
-        new Thread(() -> {
-            while (true) {
-                try {
-                    purgeExpiredTokens();
-                    Thread.sleep(PURGE_DELAY);
-                } catch (InterruptedException e) {
-                    return;
-                }
-            }
-        }).start();
+        Executors
+            .newSingleThreadScheduledExecutor((r) -> new Thread(r, "Session Manager"))
+            .scheduleAtFixedRate(
+                this::purgeExpiredTokens,
+                0, PURGE_DELAY, TimeUnit.MILLISECONDS
+            );
         LOGGER.info("Service started");
     }
 
-    private void purgeExpiredTokens() throws InterruptedException {
+    private void purgeExpiredTokens() {
         LOGGER.info("Purging outdated tokens");
         var logins = repository.allLogins().get();
         var limit = LocalDateTime.now().minusDays(TOKEN_LIFETIME_IN_DAYS);
         for (var login : logins) {
-            if (Thread.interrupted()) throw new InterruptedException();
             if (login.lastRefresh().isBefore(limit)) {
                 repository.logout(login.token());
             }
