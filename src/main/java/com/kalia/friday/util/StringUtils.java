c6fadccd38d4cd152611451819547e54a1a4c4bc
zwendo <lorris.creantor@gmail.com>
2021-11-14 13:19:16 +0100

    Added doc on StringUtils.

--- src/main/java/com/kalia/friday/util/StringUtils.java
+++ src/main/java/com/kalia/friday/util/StringUtils.java
@@ -2,14 +2,32 @@ package com.kalia.friday.util;
 
 import static java.util.Objects.requireNonNull;
 
+/**
+ * Class that contains string utils methods.
+ */
 public final class StringUtils {
     private StringUtils() {}
 
+    /**
+     * Asserts that a string is not null and not blank.
+     *
+     * @param s the string to test
+     * @return the string
+     * @throws NullPointerException if string is null
+     * @throws IllegalArgumentException if string is blank
+     */
     public static String requireNotEmpty(String s) {
         requireNonNull(s);
         return requireNotBlank(s);
     }
 
+    /**
+     * Asserts that a string is not blank (it could be null).
+     *
+     * @param s the string to test
+     * @return the string
+     * @throws IllegalArgumentException if string is blank
+     */
     public static String requireNotBlank(String s) {
         if (s != null && s.isEmpty()) {
             throw new IllegalArgumentException("string is empty");
