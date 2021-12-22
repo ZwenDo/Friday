c324d37cdb987bcfe17a8d2de6c3df4e202a2c36
notKamui <teillard.jimmy@outlook.fr>
2021-12-22 18:26:35 +0100

    Added endpoint links to delete and update a user.

--- src/main/ui/src/repositories/user_repository.js
+++ src/main/ui/src/repositories/user_repository.js
@@ -1,5 +1,6 @@
 import {loginUser} from "./login_repository";
 import {booleanHTTPRequest} from "../utils/http_requests";
+import {getCookie} from "../utils/cookies";
 
 const api = "/api/users/";
 
@@ -13,5 +14,28 @@ export function registerUser(username, password) {
         },
         201,
         _ => loginUser(username, password)
+    );
+}
+
+export function deleteUser(password) {
+    return booleanHTTPRequest(
+        api + "delete/" + getCookie("friday-userId"),
+        "DELETE",
+        {
+            password
+        },
+        204
+    );
+}
+
+export function updateUserPassword(oldPassword, newPassword) {
+    return booleanHTTPRequest(
+        api + "update/" + getCookie("friday-userId"),
+        "PUT",
+        {
+            oldPassword,
+            newPassword
+        },
+        204
     );
 }
