bc267f0cf86f919ec246f42927f4d29f56674b61
notKamui <teillard.jimmy@outlook.fr>
2021-12-30 12:48:36 +0100

    Fixed a bug where background clip wouldn't work in chrome

--- src/main/ui/src/Tailwindcss.svelte
+++ src/main/ui/src/Tailwindcss.svelte
@@ -1,3 +1,6 @@
+<script>
+    import 'tippy.js/dist/svg-arrow.css';
+</script>
 <style global>
     /* noinspection CssInvalidAtRule */
     @tailwind base;
@@ -7,12 +10,21 @@
     @tailwind utilities;
 
     @layer base {
-        .clip-path-slant-up-right {
-            clip-path: polygon(0% 0%, 100% 0%, 100% 10%, 0% 50%);
+        .tippy-box[data-theme~='friday'] {
+            @apply bg-purple-500 text-white px-2 py-[2px] rounded-lg;
         }
 
-        .bg-brand {
-            @apply bg-gradient-to-r from-purple-900 via-purple-700 to-pink-400;
+        .tippy-box[data-theme~='friday'] > .tippy-svg-arrow {
+            @apply fill-current text-purple-500
+        }
+
+        .tippy-box[data-animation='friday'][data-placement^=top] {
+            transform-origin: bottom
+        }
+
+        .tippy-box[data-animation='friday'][data-state=hidden] {
+            transform: scale(0);
+            opacity: .25
         }
     }
 
@@ -22,6 +34,21 @@
             shadow-md border-gray-100 outline-none
             focus:ring-purple-200 focus:ring-2
             transition-all;
+        }
+    }
+
+    @layer utilities {
+        .clip-path-slant-up-right {
+            clip-path: polygon(0% 0%, 100% 0%, 100% 10%, 0% 50%);
+        }
+
+        .bg-brand {
+            @apply bg-gradient-to-r from-purple-900 via-purple-700 to-pink-400;
+        }
+
+        .bg-clip-text {
+            background-clip: text;
+            -webkit-background-clip: text;
         }
     }
 </style>
