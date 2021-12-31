efdb67d907f9f00ed3f58cf6f6b6e5ce35d8560b
notKamui <teillard.jimmy@outlook.fr>
2021-12-31 12:18:05 +0100

    Added directions with Google Map API

--- src/main/ui/src/subparts/EventDetails.svelte
+++ src/main/ui/src/subparts/EventDetails.svelte
@@ -5,6 +5,7 @@
     import Heading from "../components/Heading.svelte";
     import Details from "../components/Details.svelte";
     import EventForm from "./EventForm.svelte";
+    import {deleteEvent} from "../stores/event_store";
 
     const {open, close} = getContext('simple-modal');
 
@@ -18,6 +19,26 @@
 
     const end = new Date(event.end);
     const actualEnd = `${end.toLocaleDateString()} ${end.toLocaleTimeString()}`;
+
+    const userOrigin = {
+        latitude: null,
+        longitude: null
+    };
+    if (event.place || (event.latitude && event.longitude)) {
+        navigator.geolocation.getCurrentPosition(position => {
+            userOrigin.latitude = position.coords.latitude;
+            userOrigin.longitude = position.coords.longitude;
+        });
+    }
+
+    let location = null;
+    if (event.latitude && event.longitude) {
+        location = `${event.latitude},${event.longitude}`;
+    } else if (event.place) {
+        location = event.place;
+    }
+
+    let mapurl = `https://www.google.com/maps/embed/v1/directions?mode=transit&origin=${userOrigin.latitude},${userOrigin.longitude}&destination=${location}&key=${process.env.GMAP_KEY}`;
 
     function gotoEdit() {
         close();
@@ -33,6 +54,13 @@
                 overflowY: 'scroll',
             }
         });
+    }
+
+    function gotoDelete() {
+        deleteEvent(event.id, _ => {
+            calendarRefs.forEach(c => c.getAPI().refetchEvents());
+            close();
+        })
     }
 </script>
 
@@ -56,14 +84,22 @@
             {/if}
         </div>
     </div>
-    {#if event.latitude && event.longitude}
-        <Details extendClass="mt-4" name="Map">
-            <iframe src="https://hin.slama.io"></iframe>
-        </Details>
-    {/if}
+    <Details extendClass="mt-4" name="Map">
+        {#if location && userOrigin.latitude && userOrigin.longitude}
+            <iframe title="Event map"
+                    class="w-full" height="500"
+                    style="border:0" frameborder="0"
+                    src={mapurl} allowfullscreen></iframe>
+        {:else}
+            <p class="m-7 text-lg font-thin">
+                Cannot get directions :
+                {#if !location} no location specified for this event{:else} cannot get your location{/if}
+            </p>
+        {/if}
+    </Details>
     <div class="w-full h-full flex justify-start items-center mt-4">
         <Button extendClass="bg-pink-500 hover:bg-pink-600 mr-2" on:click={close}>Close</Button>
         <Button extendClass="bg-purple-500 hover:bg-purple-600 mr-2" on:click={gotoEdit}>Edit</Button>
-        <Button extendClass="bg-red-600 hover:bg-red-700 mr-2" on:click={close}>Delete</Button>
+        <Button extendClass="bg-red-600 hover:bg-red-700 mr-2" on:click={gotoDelete}>Delete</Button>
     </div>
 </div>
